7e4353268ddc7b074076c2a0f7dd42b29453ca55
Gin <2120503549@qq.com>
2026-05-10 12:52:30 +0800

    串口双向通信测试

--- Software/main.c
+++ Software/main.c
@@ -1,1 +1,29 @@
-
+#include <reg52.h>
+void UART_Init()
+{
+    TMOD |= 0x20;
+    TH1 = 0xF9; // 12MHz晶振，2400波特率
+    TL1 = 0xF9;
+    TR1 = 1;
+    SCON = 0x50;
+}
+void UART_SendByte(unsigned char dat)
+{
+    SBUF = dat;
+    while(!TI);
+    TI = 0;
+}
+void main()
+{
+    unsigned char recv;
+    UART_Init();
+    while(1)
+    {
+        if(RI)
+        {
+            RI = 0;
+            recv = SBUF;
+            UART_SendByte(recv); // 收到什么发回什么
+        }
+    }
+}
